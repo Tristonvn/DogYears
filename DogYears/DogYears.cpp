75b5e390311ddb0712af4cb5e58d5746f5fb7e31
Tristonvn <tristonvn@gmail.com>
2021-04-12 22:33:12 +0200

    completed project

--- DogYears/DogYears.cpp
+++ DogYears/DogYears.cpp
@@ -1,9 +1,12 @@
-
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int dog_age = 8;
+    int early_years = 21; //First two years of a dogs life count as 21 years
+    int later_years = (dog_age -2)*4; //Each following year counts as 4 human years
+    int human_years = early_years + later_years;
+
+    std::cout << "My name is Jack! Ruff ruff, I am " << human_years << " years old in human years.";
 }
 
